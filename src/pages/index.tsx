a28f18f292610ac3a7d1072d5c8a545c5836eaa8
nelsoneo <nelsoneo@gmail.com>
2021-10-09 19:27:17 -0300

    more style component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from 'next'
 import SafeEnvironment from 'ui/components/feedback/safeEnvironment/SafeEnvironment'
+import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 
 
 const Home: NextPage = () => {
   return (
     <div>
       <SafeEnvironment />
+      <PageTitle 
+        title = {'Conheça os Professionais'}
+        subtitle = {'Preencha seu endereço  e veja os professionais da sua localidade'}
+      />
     </div>
   )
 }
