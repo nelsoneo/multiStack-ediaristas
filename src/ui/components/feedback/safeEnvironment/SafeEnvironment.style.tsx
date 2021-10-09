9702325c85d0c8db8686ad2bb8bc8ea07b4637fd
nelsoneo <nelsoneo@gmail.com>
2021-10-09 17:46:49 -0300

    components styles e container

--- src/ui/components/feedback/safeEnvironment/SafeEnvironment.style.tsx
+++ src/ui/components/feedback/safeEnvironment/SafeEnvironment.style.tsx
@@ -1,8 +1,6 @@
 import { styled } from '@mui/material/styles';
 
 export const SafeEnvironmentContainer = styled('div')`
-    background-color: red;
-    color: white;
     text-align: right;
     padding: 16px 0;
     font-size: 12px;
--- src/ui/components/feedback/safeEnvironment/SafeEnvironment.tsx
+++ src/ui/components/feedback/safeEnvironment/SafeEnvironment.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { SafeEnvironmentContainer } from "./SafeEnvironment.style";
+import { Container } from '@mui/material';
 
 const SafeEnvironment = () => {
     return (
          <SafeEnvironmentContainer>
-             Ambiente Seguro
+           <Container>
+                Ambiente Seguro
+           </Container>
          </SafeEnvironmentContainer>   
     );
 };
