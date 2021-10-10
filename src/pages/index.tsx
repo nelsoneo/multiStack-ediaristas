3f362bcdda4a0f5f6f314fd50a14bedc52b6a7bd
nelsoneo <nelsoneo@gmail.com>
2021-10-10 14:13:43 -0300

    add footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,19 +8,29 @@ const Home: NextPage = () => {
   return (
     <div>
       <SafeEnvironment />
-      <PageTitle 
-        title = {'Conheça os Professionais'}
-        subtitle = {'Preencha seu endereço  e veja os professionais da sua localidade'}
+      <PageTitle
+        title={"Conheça os Professionais"}
+        subtitle={
+          "Preencha seu endereço  e veja os professionais da sua localidade"
+        }
       />
 
       <UserInformation
-        name={'Nelson Hernandez'}
-        picture={'https://github.com/nelsoneo.png'}
+        name={"Nelson Hernandez"}
+        picture={"https://github.com/nelsoneo.png"}
         rating={4}
-        description={'Rio Grande do Sul'}
+        description={"Rio Grande do Sul"}
+      />
+      <UserInformation
+        name={"Daniel Hernandez Puig"}
+        picture={
+          "img/danielMySon.png"
+        }
+        rating={5}
+        description={"La Habana"}
       />
     </div>
-  )
+  );
 }
 
 export default Home
