0f29a39e2256698b188b63a4090b12f4f15243cb
nelsoneo <nelsoneo@gmail.com>
2021-10-10 16:04:21 -0300

    add textfield style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import SafeEnvironment from 'ui/components/feedback/safeEnvironment/SafeEnvironment'
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
+import TextField from 'ui/components/inputs/TextField/TextField';
 
 
 const Home: NextPage = () => {
@@ -14,6 +15,8 @@ const Home: NextPage = () => {
           "Preencha seu endereço  e veja os professionais da sua localidade"
         }
       />
+
+      <TextField />
 
       <UserInformation
         name={"Nelson Hernandez"}
