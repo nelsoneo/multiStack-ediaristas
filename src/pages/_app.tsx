50cb468ca3a756682bd8eb07f3270d71e2a8ea0c
nelsoneo <nelsoneo@gmail.com>
2021-10-10 15:51:10 -0300

    footer container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,8 @@ import { ThemeProvider } from '@mui/material';
 import theme from 'ui/themes/theme';
 import Head from 'next/head';
 import Header from 'ui/components/surfaces/Header/Header';
-
+import Footer from "ui/components/surfaces/Footer/Footer";
+import { AppContainer } from "styles/pages/_app.style";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -31,8 +32,11 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
 
       <ThemeProvider theme={theme}>
-        <Header />
-        <Component {...pageProps} />
+        <AppContainer>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </AppContainer>
       </ThemeProvider>
     </>
   );
