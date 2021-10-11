468947517a7370543ed406a7e708dee0ff86de16
nelsoneo <nelsoneo@gmail.com>
2021-10-10 22:21:18 -0300

    interface

--- src/data/hooks/pages/useIndex.page.ts
+++ src/data/hooks/pages/useIndex.page.ts
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import { UserShorInterface } from "data/@types/UserInterface";
 
 export default function useIndex() {
-    const [cep, setCep] = useState(''),
-        [erro, setErro] = useState(''),
-        [buscaFeita, setBuscaFeita] = useState(false),
-        [carregando, setCarregando] = useState(false),
-        [diarista, setDiarista] = useState([]);
+    const [cep, setCep] = useState(""),
+      [erro, setErro] = useState(""),
+      [buscaFeita, setBuscaFeita] = useState(false),
+      [carregando, setCarregando] = useState(false),
+      [diarista, setDiarista] = useState([] as UserShorInterface[]);
 
     return {
         cep,
